b40940db12c3105f4668deb83d0dbed027d1aede
poofytoo <victorhung92@gmail.com>
2014-10-21 15:33:50 -0400

    added hello page, added refresh functionality

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,1 +1,13 @@
-// Your client side JavaScript here
+$(function() {
+  var ref = new Firebase('https://poofytoo.firebaseio.com/infoloungelet');
+
+  // Force Reset Page
+  ref.child('refresh').on('value', function(s) {
+    console.log(s.val());
+    if (s.val() == 1) {
+      ref.child('refresh').set(0);
+      location.reload();
+    }
+  })
+
+});
